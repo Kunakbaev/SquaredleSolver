2f9785b414e33e1039c397d53e136c15803d301a
Kunakbaev <53218382+Kunakbaev@users.noreply.github.com>
2024-05-06 15:13:58 +0300

    Update ScrappingBoard.py

--- ScrappingBoard.py
+++ ScrappingBoard.py
@@ -6,8 +6,9 @@ import pyautogui
 import pyppeteer
 import solveBoard
 
-
+# side of board
 N = 4
+# website URL
 URL = "https://squaredle.app/"
 
 async def main():
@@ -23,12 +24,11 @@ async def main():
     await page.goto(URL)
 
     board = await page.querySelector("div .board")
-    # tmp = await page.evaluate("(board) => board.textContent", board)
-
     letters = ""
     li = await board.querySelectorAll("div .unnecessaryWrapper")
     cells = [[] for i in range(N)]
     for i in range(len(li)):
+        # position of element on web page
         tmp = await page.evaluate("""
         (obj) => {
             var x = obj.offsetLeft;
@@ -45,6 +45,7 @@ async def main():
         }
         """, li[i])
 
+        # letter that tile represents (can be space)
         ch = await page.evaluate("""(li) => li.textContent""", li[i])
         letters += ch
         cells[i // N].append((tmp['x'], tmp['y']))
@@ -67,35 +68,29 @@ async def main():
         tmp = letters[N * i: N * (i + 1)].lower()
         matrix.append(tmp)
 
-    # 545 and 430 are coordinates of the center of the top left tile of the matrix in the screen
+    X, Y = 545, 430
+    # X and Y + position of element on webpage are coordinates
+    # of the center of the top left tile of the matrix in the screen
     # BE is distance between center's of two neighbour tiles
     BE = 0
     for i in range(N):
         BE += cells[i][-1][0] - cells[i][0][0]
     BE //= (N * (N - 1))
-    print(BE)
     wordsInTable = solveBoard.findWordsInTable(matrix)
-    print(len(wordsInTable))
+
+    # output all found words via mouse lib
     for word, path in wordsInTable.items():
         x, y = path[0]
-        # print(word, path)
-        pyautogui.moveTo(545 + y * BE, 430 + x * BE)
+        pyautogui.moveTo(X + y * BE, Y + x * BE)
         pyautogui.mouseDown()
+        # drag mouse along tile that are in path
         for i in range(1, len(path)):
             x, y = path[i]
-
-            pyautogui.moveTo(545 + y * BE, 430 + x * BE, duration=0.3)
+            pyautogui.moveTo(X + y * BE, Y + x * BE, duration=0.3)
         pyautogui.mouseUp()
-
-        # exit(0)
     time.sleep(1)
 
     await browser.close()
 
-
-# while True:
-#     print(pyautogui.position())
-
 asyncio.get_event_loop().run_until_complete(main())
-
 
